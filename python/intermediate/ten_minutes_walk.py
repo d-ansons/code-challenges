c8d9c59920fd2a3925eafc5644b0936d1997d6b0
Davis <davis.ansons@outlook.com>
2026-07-14 21:42:01 +0100

    clear repo

--- python/intermediate/ten_minutes_walk.py
+++
@@ -1,6 +0,0 @@
-# Take a Ten Minutes Walk
-
-# Create a function that will return ture if the walk is exactly ten minutes and returns you to your starting point.
-
-def is_valid_walk(walk):
-    return walk.count('n') == walk.count('s') and walk.count('e') == walk.count('w') and len(walk) == 10
