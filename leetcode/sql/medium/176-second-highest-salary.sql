78a16246e6a01e3e08e79f8468ee2250f1b2ddb7
Davis <davis.ansons@outlook.com>
2026-07-15 12:05:02 +0100

    second highest salary challenge

---
+++ leetcode/sql/medium/176-second-highest-salary.sql
@@ -0,0 +1,14 @@
+/*
+Write a solution to find the second highest distinct salary from the Employee table. If there is no second highest salary, return null (return None in Pandas).
+
+The result format is in the following example.
+*/
+
+SELECT (
+    SELECT DISTINCT salary
+    FROM Employee
+    ORDER BY salary DESC
+    LIMIT 1
+    OFFSET 1
+) AS SecondHighestSalary
+;
