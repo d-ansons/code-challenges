f2817ab4353b87f0574a07630b2bc0bd6c3d69a4
Davis <davis.ansons@outlook.com>
2026-07-16 15:33:11 +0100

    customers who never order challenge

---
+++ leetcode/sql/easy/183-customers-who-never-order.sql
@@ -0,0 +1,14 @@
+/*
+Write a solution to find all customers who never order anything.
+
+Return the result table in any order.
+*/
+
+-- Write your PostgreSQL query statement below
+SELECT
+    c.name AS Customers
+FROM Customers c
+LEFT JOIN Orders o
+    ON c.id = o.customerId
+WHERE o.id IS NULL
+;
