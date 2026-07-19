d72b2c469365691ad5d1a9c00b1db931f83e175a
Davis <davis.ansons@outlook.com>
2026-07-19 20:56:11 +0100

    tree node challenge : to revisit

---
+++ leetcode/sql/medium/608-tree-node.sql
@@ -0,0 +1,18 @@
+/*
+Each node in the tree can be one of three types:
+
+"Leaf": if the node is a leaf node.
+"Root": if the node is the root of the tree.
+"Inner": If the node is neither a leaf node nor a root node.
+Write a solution to report the type of each node in the tree.
+*/
+
+-- Write your PostgreSQL query statement below
+SELECT
+    id,
+    CASE
+        WHEN p_id IS NULL THEN 'Root'
+        WHEN id IN (SELECT p_id FROM Tree) THEN 'Inner'
+        ELSE 'Leaf'
+    END AS type
+FROM Tree
