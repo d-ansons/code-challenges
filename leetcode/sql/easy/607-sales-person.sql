6597133f1b790d965c74b71a8d79365918ab64e7
Davis <davis.ansons@outlook.com>
2026-07-19 20:38:16 +0100

    sales person challenge

---
+++ leetcode/sql/easy/607-sales-person.sql
@@ -0,0 +1,21 @@
+/*
+Write a solution to find the names of all the salespersons who did not have any orders related to the company with the name "RED".
+
+Return the result table in any order.
+*/
+
+-- Write your PostgreSQL query statement below
+SELECT
+    sp.name AS name
+FROM SalesPerson sp
+WHERE sp.name NOT IN (
+    SELECT 
+        sp.name
+    FROM SalesPerson sp
+    LEFT JOIN Orders o
+        ON sp.sales_id = o.sales_id
+    LEFT JOIN Company c
+        ON o.com_id = c.com_id
+    WHERE c.name = 'RED'
+)
+;
