965a243ce3fafaf7eaf180791e6b7d04a26ff7b9
Davis <davis.ansons@outlook.com>
2026-07-19 12:52:07 +0100

    disemvowel trolls challenge

---
+++ codewars/7th-kyu/disemvowel-trolls.sql
@@ -0,0 +1,7 @@
+-- # write your SQL statement here: you are given a table 'disemvowel' with column 'str', 
+-- # return a table with column 'str' and your result in a column named 'res'.
+SELECT
+  str,
+  REGEXP_REPLACE(str, '[aeiouAEIOU]', '', 'g') AS res
+FROM disemvowel
+;
